66e6e9e4eeab0c56c6a9c0489757fdf5ab88b991
Maruf Tuhin <maruf.2hin@gmail.com>
2018-11-02 15:36:39 +0600

    Add CRDs without observation when operator starts (#94)
    xref: https://github.com/kubedb/project/issues/310

--- hack/deploy/setup.sh
+++ hack/deploy/setup.sh
@@ -48,7 +48,7 @@ source "$REPO_ROOT/hack/libbuild/common/lib.sh"
 
 export KUBE_CA=$($ONESSL get kube-ca | $ONESSL base64)
 export APPSCODE_ENV=${APPSCODE_ENV:-prod}
-export KUBEDB_SCRIPT="curl -fsSL https://raw.githubusercontent.com/kubedb/cli/0.8.0/"
+export KUBEDB_SCRIPT="curl -fsSL https://raw.githubusercontent.com/kubedb/cli/0.9.0-rc.0/"
 
 show_help() {
   echo "setup.sh - setup kubedb operator"
